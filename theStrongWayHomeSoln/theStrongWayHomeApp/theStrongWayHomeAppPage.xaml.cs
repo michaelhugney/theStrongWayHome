08c8769a7bffe6a917eac26757aa40ca5b8b952b
Michael Hugney <mhugney@vt.edu>
2016-06-21 01:40:51 -0400

    Added a message and a button

--- theStrongWayHomeSoln/theStrongWayHomeApp/theStrongWayHomeAppPage.xaml.cs
+++ theStrongWayHomeSoln/theStrongWayHomeApp/theStrongWayHomeAppPage.xaml.cs
@@ -7,6 +7,27 @@ namespace theStrongWayHomeApp
 		public theStrongWayHomeAppPage()
 		{
 			InitializeComponent();
+
+			Button button = new Button { Text = "Let's Go" };
+			button.Clicked += async (sim, e) =>
+			{
+				await this.DisplayAlert("Alert", "You clicked me!", "OK");
+			};
+
+			var layout = new StackLayout
+			{
+				VerticalOptions = LayoutOptions.Center,
+
+				Children =
+				{
+					new Label
+					{
+						HorizontalTextAlignment = TextAlignment.Center,
+						Text = "Ready to take the Strong Way Home?"
+					}, button
+				}
+			};
+			Content = layout;
 		}
 	}
 }
